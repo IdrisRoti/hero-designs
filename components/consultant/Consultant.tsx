7e4b0e2d92bfdcc07a73e0032ffbaaa1b2aea60e
IdrisRoti <idrisomisakin@gmail.com>
2024-12-03 09:29:16 +0100

    fix: [consultation] - increase animation duration

--- components/consultant/Consultant.tsx
+++ components/consultant/Consultant.tsx
@@ -58,12 +58,12 @@ const Consultant = () => {
         <div className="absolute w-[85%] md:w-[75%] lg:w-1/2 h-full top-0 right-0 flex items-start justify-end">
             {
                 gradientsArr.map(({id, gradient,delay}) => (
-                    <motion.div initial={{opacity: .6}} animate={{opacity: 1}} transition={{duration: 2, ease: "easeInOut", delay: delay, repeat: Infinity, repeatType: "reverse"}} key={id} className={`w-full bg-gradient-to-b h-full ${gradient}`} />
+                    <motion.div initial={{opacity: .5}} animate={{opacity: 1}} transition={{duration: 2, ease: "easeInOut", delay: delay, repeat: Infinity, repeatType: "reverse"}} key={id} className={`w-full bg-gradient-to-b h-full ${gradient}`} />
                 ))
             }
         </div>
         <div className="relative max-w-[75rem] h-full mx-auto">
-            <motion.nav initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .5, delay: 1.5}} className="absolute top-6 left-0 w-full">
+            <motion.nav initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .8, delay: 2}} className="absolute top-6 left-0 w-full">
                 <div className="flex justify-between max-w-[75rem] mx-auto shadow-[0_25px_50px_-12px_rgb(0_0_0/0.15)] py-2 px-4 rounded-full bg-white/30 backdrop-blur-xl">
                     <div className="flex items-center gap-4">
                         <span className="bg-[#1e1e1e] w-6 aspect-square rounded-full grid place-items-center">
@@ -90,9 +90,9 @@ const Consultant = () => {
             </motion.nav>  
             <div className="absolute w-full bottom-0 left-0 flex flex-col gap-4 md:gap-0 text-[#1e1e1e]">
                 <div className="w-full">
-                    <motion.h1 initial={{y: -20, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: .8}} className="text-[#1e1e1e]/80 capitalize md:text-xl tracking-wider">Award winning business consultant</motion.h1>
-                    <motion.h1 initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .5, delay: .7}} className="text-3xl md:text-5xl w-[90%] lg:w-[65%] mt-4 mb-12">Our experts will build you a next level strategy to accelerate your e-commerce & grow your business.</motion.h1>
-                    <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .5, delay: 1.5}} className="flex items-center gap-4">
+                    <motion.h1 initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .8}} className="text-[#1e1e1e]/80 capitalize md:text-xl tracking-wider">Award winning business consultant</motion.h1>
+                    <motion.h1 initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .8, delay: 1}} className="text-3xl md:text-5xl w-[90%] lg:w-[65%] mt-4 mb-12">Our experts will build you a next level strategy to accelerate your e-commerce & grow your business.</motion.h1>
+                    <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .8, delay: 2}} className="flex items-center gap-4">
                         <button className="flex items-center gap-2 bg-orange-600 hover:bg-orange-500 transition p-3 rounded-full text-white group">
                             <span className="uppercase text-sm font-extralight opacity-90">get started now</span>
                             <span className="bg-white rounded-full grid place-items-center w-7 aspect-square">
@@ -102,7 +102,7 @@ const Consultant = () => {
                         <UnderlinedButton>learn more</UnderlinedButton>
                     </motion.div>
                 </div>
-                <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .5, delay: 1.5}} className="relative w-44 h-32 border-4 border-orange-600 rounded-md aspect-square  ml-auto mb-8 after:absolute after:inset-0 after:bg-black/10">
+                <motion.div initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{duration: .8, delay: 2}} className="relative w-44 h-32 border-4 border-orange-600 rounded-md aspect-square  ml-auto mb-8 after:absolute after:inset-0 after:bg-black/10">
                     <Image
                         src="/photography/photo1.png"
                         alt="A smiling woman"
