90e485ddfc9bf1279ce54407d7bdccf5b26ed675
IdrisRoti <idrisomisakin@gmail.com>
2024-12-10 18:26:11 +0100

    fix: [notion] - replace sidebar Nav links / link with #

--- components/notion/SidebarNavLinks.tsx
+++ components/notion/SidebarNavLinks.tsx
@@ -9,7 +9,7 @@ const SidebarNavLinks = () => {
           <Link
             className='flex items-center gap-3 last-of-type:mb-0 hover:bg-stone-200/50 px-2 py-1.5 transition rounded-md' 
             key={id} 
-            href="/">
+            href="#">
             {/* <div className='size-2 bg-stone-500 rounded-full' /> */}
             <Icon className='text-stone-400 size-5' />
             <span>{label}</span>
